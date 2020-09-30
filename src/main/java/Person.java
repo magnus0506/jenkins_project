addf0e9fe097bdd1adde63204dee6f9135a4eb0f
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2020-10-01 01:55:42 +0200

    first commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,4 +2,10 @@ public class Person {
     String name;
     int age;
     String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
